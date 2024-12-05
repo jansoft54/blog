01cc953eb2e6034f4b245a34641f01107528ba91
FH Systems <mail@fh-systems.de>
2024-12-05 20:00:05 +0100

    include prod build

--- next.config.js
+++ next.config.js
@@ -54,17 +54,16 @@ const securityHeaders = [
   },
 ]
 
-const output = process.env.EXPORT ? 'export' : undefined
 const basePath = process.env.BASE_PATH || undefined
-const unoptimized = process.env.UNOPTIMIZED ? true : undefined
 
 /**
  * @type {import('next/dist/next-server/server/config').NextConfig}
  **/
 module.exports = () => {
   const plugins = [withContentlayer, withBundleAnalyzer]
+
   return plugins.reduce((acc, next) => next(acc), {
-    output: 'standalone', // Ensures the server files are output correctly
+    output: undefined, // Ensures the server files are output correctly
     basePath,
     reactStrictMode: true,
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
@@ -78,16 +77,8 @@ module.exports = () => {
           hostname: 'picsum.photos',
         },
       ],
-      unoptimized,
     },
-    async headers() {
-      return [
-        {
-          source: '/(.*)',
-          headers: securityHeaders,
-        },
-      ]
-    },
+
     webpack: (config, options) => {
       config.module.rules.push({
         test: /\.svg$/,
